588912f74e5673a56e304ab5dc9c59b8d4ec5ecc
wcdeich4 <wcdeich4@hotmail.com>
2026-01-10 16:10:17 -0500

    comment fix

--- WebAPI/FileRecord.cs
+++ WebAPI/FileRecord.cs
@@ -2,8 +2,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 /// <summary>
 /// C# and F# support records which are compared by value and entirely immutable by default.
-/// VB.NET does not support records without a NuGet package to convert a struct to a record, 
-/// therefore this record typd is here.
+/// VB.NET does not support records without an extra NuGet package, so this record type is here.
 /// </summary>
 /// <param name="Name">The primary way to make a record constructor is with "Positional 
 /// Record Syntax" where you only give the name and type and the properties are automaticaly
